862ef6474741481b2dcf6fa2f86122bbba6e54e8
DebisU Uchiha <davis5593@hotmail.com>
2017-08-11 15:23:55 +0200

    Add project files.

---
+++ Entty Framework Model First/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using Entty_Framework_Model_First.Persistance;
+using System;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Entity_Framework_Model_First
+{
+
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadToGrid();
+        }
+
+        private int TransformStringIntoInt(string input)
+        {
+            int output;
+            int.TryParse(input, out output);
+            return output;
+        }
+
+        private void BtInsert_Click(object sender, RoutedEventArgs e)
+        {
+            model_first_migration_testEntities testcontext = new model_first_migration_testEntities();
+            try
+            {
+
+                employee emp = new employee
+
+                {
+                    Name = TbName.Text,
+                    BirthYear = TransformStringIntoInt(TbBirthtYear.Text)
+                };
+                testcontext.employee.Add(emp);
+                testcontext.SaveChanges();
+                MessageBox.Show("Record Inserted successfully.");
+                LoadToGrid();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.InnerException.ToString());
+            }
+        }
+
+        private void BtUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            int EmpId = TransformStringIntoInt(TbId.Text);
+
+            model_first_migration_testEntities testcontext = new model_first_migration_testEntities();
+            try
+            {
+                employee emp = testcontext.employee.First(i => i.ID == EmpId);
+                {
+                    emp.Name = TbName.Text;
+
+                    int birth;
+                    string b = TbBirthtYear.Text;
+                    int.TryParse(b, out birth);
+                    emp.BirthYear = birth;
+
+                    testcontext.SaveChanges();
+                    MessageBox.Show("Record Updated successfully.");
+                    LoadToGrid();
+                };
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.InnerException.ToString());
+            }
+        }
+
+        private void BtDelete_Click(object sender, RoutedEventArgs e)
+        {
+            int EmpId = TransformStringIntoInt(TbId.Text);
+
+            model_first_migration_testEntities testcontext = new model_first_migration_testEntities();
+            try
+            {
+                employee emp = testcontext.employee.FirstOrDefault(i => i.ID == EmpId);
+                testcontext.employee.Remove(emp);
+                testcontext.SaveChanges();
+                MessageBox.Show("Record Deleted successfully.");
+                LoadToGrid();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.InnerException.ToString());
+            }
+        }
+
+        private void LoadToGrid()
+        {
+            model_first_migration_testEntities testcontext = new model_first_migration_testEntities();
+            var load = from g in testcontext.employee select g;
+            if (load != null)
+            {
+                dataGrid.ItemsSource = load.ToList();
+            }
+        }
+
+        private void dataGrid_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            TbId.Text = ((employee)dataGrid.SelectedItem).ID.ToString();
+            TbName.Text = ((employee)dataGrid.SelectedItem).Name.ToString();
+            TbBirthtYear.Text = ((employee)dataGrid.SelectedItem).BirthYear.ToString();
+        }
+    }
+}
---
+++ Entty Framework Model First/README.md
@@ -0,0 +1,22 @@
+﻿# Entity Framework Model First Example
+
+## Steps:
+>Install xampp with mysql and run the following services:
+![Running services](https://www.codeproject.com/KB/database/426790/image001.png)
+>Create the new Database(or user your own one)
+![Creating Database] (https://www.codeproject.com/KB/database/426790/image002.png)
+>Open Visual Studio and add the following references (with NuGet package):
+![Adding references] (https://www.codeproject.com/KB/database/426790/image003.png)
+>Set MySql porperty Copy Local to true:
+![Copy Local = true] (https://www.codeproject.com/KB/database/ConnectCsharpToMysql/ConnectCsharpToMysql_AddDLL.png)
+>Add Entity data model into project:
+![add ADO .NET Entity Data Model] (https://www.codeproject.com/KB/database/426790/image005.jpg)
+>Follow the next steps of the wizzard
+![Select Generate fron database](https://www.codeproject.com/KB/database/426790/image006.png)
+![Select MySql Database](https://www.codeproject.com/KB/database/426790/image007.png)
+if the MySql Database is not available see this video:
+[![Watch the video](https://raw.github.com/GabLeRoux/WebMole/master/ressources/WebMole_Youtube_Video.png)](https://www.youtube.com/watch?v=VT38Rw86v2M)
+![Put your credentials](https://www.codeproject.com/KB/database/426790/image008.png)
+![Select all tables](https://www.codeproject.co/mKB/database/426790/image009.png)
+
+
